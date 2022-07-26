8ca9cffb56b75b7ffe8e5dff74df1e218f30218e
ASR26 <87180878+ASR26@users.noreply.github.com>
2022-07-26 20:30:33 +0200

    Update scorpion.py

--- scorpion.py
+++ scorpion.py
@@ -31,10 +31,11 @@ exifdata = image._getexif()
 # iterating over all EXIF data fields
 for tag_id in exifdata:
     # get the tag name, instead of human unreadable tag id
-    tag = TAGS.get(tag_id, tag_id)
-    data = exifdata.get(tag_id)
-    # decode bytes 
-    if isinstance(data, bytes):
-        data = data.decode()
-    print(f"{tag:25}: {data}")
+    if (tag_id != null):
+        tag = TAGS.get(tag_id, tag_id)
+        data = exifdata.get(tag_id)
+        # decode bytes 
+        if isinstance(data, bytes):
+            data = data.decode()
+        print(f"{tag:25}: {data}")
     
